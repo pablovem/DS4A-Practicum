f8f441b7bd294853631d517d3c11bca245645a17
Pablovem <pablovema@gmail.com>
2020-07-28 00:44:01 -0500

    feat: add routes to the app

--- app.py
+++ app.py
@@ -1,35 +1,38 @@
-import math
-import numpy as np
-import datetime as dt
-import pandas as pd
-import json
-
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import plotly.express as px
-import pandas as pd
+from dash.dependencies import Input, Output, State
 
 from lib import header, stats_pac
 
 app = dash.Dash(
     __name__, 
-    meta_tags=[{"name": "viewport", "content": "width=device-width"}]
+    meta_tags=[{"name": "viewport", "content": "width=device-width"}],
+    suppress_callback_exceptions=True
 )
 
 app.layout = html.Div(
     [
+        dcc.Location(id="url", refresh=False),
         header.header,
-        html.Div(
-            [
-                stats_pac.stats_pac,
-            ],
-            id="report",
-            className="row",
-        )
+        html.Div(id="report",className="row")
     ],
     id="report-container",
 )
+
+index_page = html.Div([
+    html.H1('DS4A')
+])
+
+@app.callback(
+    dash.dependencies.Output('report','children'),
+    [dash.dependencies.Input('url','pathname')]
+)
+def render_page(pathname):
+    if pathname == '/daniel':
+        return stats_pac.stats_pac,
+    else:
+        return index_page
 
 if __name__ == '__main__':
     app.run_server(debug=True)
